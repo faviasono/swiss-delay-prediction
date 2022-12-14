7947ce05ee1590c3abe4c4070c4609353c29d76c
faviasono <andrea.faviait@gmail.com>
2022-12-14 09:26:41 +0100

    Refactor code

--- src/multiprocessing_previous_flight_same_day_delayed.py
+++ src/multiprocessing_previous_flight_same_day_delayed.py
@@ -2,30 +2,27 @@ import pandas as pd
 from multiprocessing import cpu_count, pool
 
 merged = pd.read_csv("/Users/favea/Downloads/swiss-data/merged_df.csv")
-merged.wh_fleg_dep_day_scd = pd.to_datetime(
-    merged.wh_fleg_dep_day_scd, dayfirst = True
-)
+merged.wh_fleg_dep_day_scd = pd.to_datetime(merged.wh_fleg_dep_day_scd, dayfirst=True)
 merged.wh_fleg_rot_leg_i_prev = merged.wh_fleg_rot_leg_i_prev.astype(float)
 merged.wh_fleg_leg_i = merged.wh_fleg_leg_i.astype(float)
 
+
 def is_previous_delayed_same_day(row):
     # Filter the DataFrame to only include rows with a departure time within one hour of the input datetime
-    previous_is_delayed  = False
+    previous_is_delayed = False
     departure_date = row["wh_fleg_dep_day_scd"]
     print(departure_date)
     filtered_data = merged[merged.wh_fleg_rot_leg_i_prev == row.wh_fleg_leg_i]
     is_null = bool(len(filtered_data.wh_fleg_leg_i))
-  
+
     if len(filtered_data.wh_fleg_leg_i) > 0:
         if filtered_data.wh_fleg_dep_day_scd.values[0] == departure_date:
             previous_is_delayed = filtered_data.is_delayed.values[0]
 
-    return dict(wh_fleg_leg_i = int(row.wh_fleg_leg_i),
-                previous_is_delayed_same_day = previous_is_delayed)
-    
-
-
-
+    return dict(
+        wh_fleg_leg_i=int(row.wh_fleg_leg_i),
+        previous_is_delayed_same_day=previous_is_delayed,
+    )
 
 
 if __name__ == "__main__":
